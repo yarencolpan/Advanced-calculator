fe518f428677bf09aed39f0aa01c8ef58dfbbc62
yaren çolpan <yarenncol8@gmail.com>
2023-09-06 14:35:38 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,54 +9,54 @@ select '6' for finding the exponent operation
 select '7' for factorial operation
 select '8' for absolute value operation
 select '9' for finding the nearest whole numbers ta a decimal number """)
-b = int(input("Select the number of the task you want to:"))
-t = 0
-h = 0
-hh = 1
-z = 1
-if b == 1:
-    # it doesn't matter how times you enter a number thanks to split :)
-    a = input("Enter the numbers you want to use for the operation, leaving one space between each.:").split()
-    for i in a:
-        t += int(i)
-    print(t)
-elif b == 2:
-    # it doesn't matter how times you enter a number thanks to split :)
-    aa = input("Enter the numbers you want to use for the operation, leaving one space between each.:").split()
-    for j in aa:
-        h = int(j) * 2
+number = int(input("Select the number of the task you want to:"))
+total = 0
+first_number = 0
+multiplications_ = 1
+first_input = 1
+if number == 1:
+    # it doesn't matter how many times you enter a number thanks to split
+    numbers_to_add = input("Enter the numbers , leaving one space between each.:").split()
+    for i in numbers_to_add:
+        total += int(i)
+    print(total)
+elif number == 2:
+    # it doesn't matter how many times you enter a number thanks to split
+    subtraction = input("Enter the numbers, leaving one space between each.:").split()
+    for j in subtraction:
+        first_number = int(j) * 2
         break
-    for p in aa:
-        h -= int(p)
-    print(h)
-elif b == 3:
-    # it doesn't matter how times you enter a number thanks to split :)
-    aaa = input("Enter the numbers you want to use for the operation, leaving one space between each.:").split()
-    for e in aaa:
-        z = int(e) * int(e)
+    for p in subtraction:
+        first_number -= int(p)
+    print(first_number)
+elif number == 3:
+    # it doesn't matter how times you enter a number thanks to split
+    division = input("Enter the numbers, leaving one space between each.:").split()
+    for e in division:
+        first_input = int(e) * int(e)
         break
-    for ee in aaa:
-        z /= int(ee)
-    print(z)
-elif b == 4:
+    for w in division:
+        first_input /= int(w)
+    print(first_input)
+elif number == 4:
     # it doesn't matter how times you enter a number thanks to split :)
-    aaa = input("Enter the numbers you want to use for the operation, leaving one space between each.").split()
-    for ii in aaa:
-        hh *= int(ii)
-    print(hh)
-elif b == 5:
-    bbb = input("Enter the number you want to use for the operation:")
-    print(math.pow(int(bbb), 0.5))
-elif b == 6:
-    ll, m = input("""Enter the numbers you want to use for the operation, leaving one space between each.
-    first-number second-exponent:""").split()
-    print(math.pow(int(ll), int(m)))
-elif b == 7:
-    gg = input("Enter the number you want to use for the operation:")
-    print(math.factorial(int(gg)))
-elif b == 8:
-    u = input("Enter the number you want to use for the operation:")
-    print(math.fabs(int(u)))
-elif b == 9:
-    uu = input("Enter the number you want to use for the operation:")
-    print("floor is", math.floor(float(uu)), "ceil is", math.ceil(float(uu)))
+    multiplication = input("Enter the numbers, leaving one space between each.").split()
+    for g in multiplication:
+        multiplication *= int(g)
+    print(multiplication)
+elif number == 5:
+    square_root = input("Enter the number you want to use for the operation:")
+    print(math.pow(int(square_root), 0.5))
+elif number == 6:
+    base, exponent = input("""Enter the numbers, leaving one space between each.
+    first-base second-exponent:""").split()
+    print(math.pow(int(base), int(exponent)))
+elif number == 7:
+    factorial = input("Enter the number you want to use for the operation:")
+    print(math.factorial(int(factorial)))
+elif number == 8:
+    absolute_value = input("Enter the number you want to use for the operation:")
+    print(math.fabs(int(absolute_value)))
+elif number == 9:
+    rounding = input("Enter the number you want to use for the operation:")
+    print("floor is", math.floor(float(rounding)), "ceil is", math.ceil(float(rounding)))
